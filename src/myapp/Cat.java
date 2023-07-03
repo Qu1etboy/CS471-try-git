a217f3b6bf755ec8b1533262242aba91092359d5
Qu1etboy <nonzagreanthai@gmail.com>
2023-07-03 11:51:13 +0700

    bring back Cat.java from commit #fcc9c49

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -1,13 +1,5 @@
 package myapp;
 
 public class Cat {
-    private String name;
 
-    public Cat(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
